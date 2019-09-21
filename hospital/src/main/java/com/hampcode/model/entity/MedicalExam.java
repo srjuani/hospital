ea23eb068fc0b3a1b25ed3fd49e876b8338906e5
Usuario <Usuario>
2019-09-21 09:46:39 -0500

    changing persistence

--- hospital/src/main/java/com/hampcode/model/entity/MedicalExam.java
+++ hospital/src/main/java/com/hampcode/model/entity/MedicalExam.java
@@ -17,6 +17,22 @@ public class MedicalExam {
 	
 	@Column(name="name", nullable=false,length=50)
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 }
